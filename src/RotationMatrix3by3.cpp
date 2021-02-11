25ae499ddaf54944886ddd10ffac232e7c98f914
AhmedHumais <m.ahmedhumais@gmail.com>
2021-02-11 16:49:07 +0400

    Transpose rotation matrix 3x3

--- src/RotationMatrix3by3.cpp
+++ src/RotationMatrix3by3.cpp
@@ -32,6 +32,7 @@ Eigen::Matrix<float, 3, 3> RotationMatrix3by3::Update(Vector3D<float> t_input)
     Matrix3by3::v3.y =sy * sp * cr - cy * sr;
     Matrix3by3::v3.z =cp * cr;
 
+    Transpose();
     /* 
     
     Original Rotation Matrix for ENU coordinate system. 
