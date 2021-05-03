087cf4741b037441d67d76a783d2bb9827abaa27
AhmedHumais <m.ahmedhumais@gmail.com>
2021-05-03 17:28:54 +0400

    added poly filter

--- include/HEAR_math/ButterFilter_2nd.hpp
+++ include/HEAR_math/ButterFilter_2nd.hpp
@@ -17,15 +17,19 @@ private:
     float coeff_120Hz_2nd_butter_5hz[5] = { -1.279632424997809,0.477592250072517,0.049489956268677,0.098979912537354,0.049489956268677 };
     float coeff_200Hz_2nd_butter[5] = { -1.97779f, 0.97803f, 6.1006e-05f, 1.2201e-04f, 6.1006e-05f };
     float coeff_200Hz_2nd_butter_50hz[5] = {-1.561018075800718, 0.641351538057563, 0.020083365564211, 0.040166731128423, 0.020083365564211};
+    float coeff_N200C90[5] = {1.5610, 0.6414, 0.8006, 1.6012, 0.8006};
+    float coeff_N200C60[5] = {0.3695, 0.1958, 0.3913,    0.7827,    0.3913};
+    float coeff_N200C50[5] = {-1.8e-16,    0.1716,    0.2929,    0.5858,    0.2929};
+    
     float _coeff_in_use[5];
     void initPorts();
     void updateCoeff(DataMsg*);
     void updateCoeff(float*);
-    void runTask(DataMsg*);
 public:
     enum ports_id {IP_0_DATA, IP_1_COEFF, OP_0_DATA};
-    enum BF_settings {FS120FC5, FS200FC10, FS200FC50};
+    enum BF_settings {FS120FC5, FS200FC10, FS200FC50, N200C50, N200C60, N200C90};
     void process(DataMsg* t_msg, Port* t_port);
+    float runTask(float);
     ButterFilter_2nd();
     ButterFilter_2nd(BF_settings t_settings);
     ~ButterFilter_2nd();
--- src/ButterFilter_2nd.cpp
+++ src/ButterFilter_2nd.cpp
@@ -16,6 +16,15 @@ ButterFilter_2nd::ButterFilter_2nd(BF_settings t_settings) {
     else if (t_settings == BF_settings::FS200FC50) {
         updateCoeff(coeff_200Hz_2nd_butter_50hz);
     }
+    else if (t_settings == BF_settings::N200C90) {
+        updateCoeff(coeff_N200C90);
+    }
+    else if (t_settings == BF_settings::N200C60) {
+        updateCoeff(coeff_N200C60);
+    }
+    else if (t_settings == BF_settings::N200C50) {
+        updateCoeff(coeff_N200C50);
+    }
 }
 
 ButterFilter_2nd::~ButterFilter_2nd() {
@@ -29,23 +38,24 @@ void ButterFilter_2nd::initPorts() {
     _ports = {_input_port_0, _input_port_1, _output_port};
 }
 
-void ButterFilter_2nd::runTask(DataMsg* t_msg) {
+float ButterFilter_2nd::runTask(float _ip_0) {
     float x=_ip_0;
 	float y = -_coeff_in_use[0] * prev_y - _coeff_in_use[1] * prev2_y + _coeff_in_use[2] * x + _coeff_in_use[3] * prev_x + _coeff_in_use[4] * prev2_x;
     prev2_y = prev_y;
 	prev_y = y;
 	prev2_x = prev_x;
 	prev_x = x;
-    FloatMsg* x_msg = new FloatMsg();
-    x_msg->data = y;
-    this->_output_port->receiveMsgData(x_msg);
+    return y;
 }
 
 void ButterFilter_2nd::process(DataMsg* t_msg, Port* t_port) {
     if(t_port->getID() == ports_id::IP_0_DATA) {
         FloatMsg* float_msg = (FloatMsg*)t_msg;
         _ip_0 = float_msg->data;
-        this->runTask(t_msg);
+        FloatMsg* x_msg = new FloatMsg();
+        x_msg->data = this->runTask(_ip_0);
+        this->_output_port->receiveMsgData(x_msg);
+
     }
     else if(t_port->getID() == ports_id::IP_1_COEFF) {
         if(t_msg->getType() == msg_type::FLOATARRAY) {
