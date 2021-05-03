c8b0696e02dddab74f09a41ae357bbec2f995b50
AhmedHumais <m.ahmedhumais@gmail.com>
2021-05-03 18:23:00 +0400

    minor bug fixes

--- src/PolyFilter.cpp
+++ src/PolyFilter.cpp
@@ -2,7 +2,7 @@
 
 PolyFilter::PolyFilter(int freq, uint8_t win_sz, uint8_t deg): _win_sz(win_sz), _dt(1.0/freq), _deg(deg) {
     this->_input_port_0 = new InputPort(ports_id::IP_0_DATA, this);
-    this->_output_port_0 = new InputPort(ports_id::OP_0_FILT, this);
+    this->_output_port_0 = new OutputPort(ports_id::OP_0_FILT, this);
     this->_output_port_1 = new OutputPort(ports_id::OP_1_FILT_DOT, this);
     _ports = {_input_port_0, _output_port_0, _output_port_1};
 
