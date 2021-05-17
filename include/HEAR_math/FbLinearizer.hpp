0d9fef944cf3a72efe3925c00f0aec4c73c18ee6
AhmedHumais <m.ahmedhumais@gmail.com>
2021-05-17 15:53:14 +0400

    fixed bug

--- include/HEAR_math/FbLinearizer.hpp
+++ include/HEAR_math/FbLinearizer.hpp
@@ -8,7 +8,6 @@
 #include "HEAR_core/logger.hpp"
 #include <tf2/LinearMath/Matrix3x3.h>
 #include <tf2/LinearMath/Quaternion.h>
-#include "HEAR_math/feedbacklinearization.hpp"
 
 #include "HEAR_core/Block.hpp"
 
